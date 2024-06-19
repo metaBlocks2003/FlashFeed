4cc75af6d608c9870605e6ae4262f27a127ea4b4
unknown <you@example.com>
2024-06-19 23:29:56 +0530

    First Commit

---
+++ src/News.jsx
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const News = () => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [categories,setCategories] = useState('general');
+  const [page, setPage] = useState(1);
+  const pageSize = 10; 
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await axios.get('https://newsapi.org/v2/top-headlines', {
+          params: {
+            page: page,
+            pageSize: pageSize,
+            category: categories,
+            country: 'us',
+            apiKey: 'b53e4c2abc75415bbf56478978c8861e'
+          }
+        });
+        console.log(`Fetched ${categories} news.}`);
+        setArticles(response.data.articles);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchArticles();
+  }, [categories, page]);
+
+  const handleCategoryChange = (newCategory) => {
+    setCategories(newCategory);
+    setLoading(true); 
+  };
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+    setLoading(true); 
+  };
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div className="p-4">
+
+      <h1 className="text-2xl font-bold mb-4">News Articles</h1>
+
+      <div className="my-10 gap-40 md:flex md:gap-5 justify-center">
+        <button onClick={() => handleCategoryChange('general')} className="mr-2 p-4 shadow-black shadow-2xl  bg-rose-800 text-white rounded-full">General</button>
+        <button onClick={() => handleCategoryChange('business')} className="mr-2 p-4 shadow-black shadow-2xl  bg-rose-800 text-white rounded-full">Business</button>
+        <button onClick={() => handleCategoryChange('technology')} className="mr-2 p-4 shadow-black shadow-2xl  bg-rose-800 text-white rounded-full">Technology</button>
+        <button onClick={() => handleCategoryChange('health')} className="mr-2 p-4 shadow-black shadow-2xl  bg-rose-800 text-white rounded-full">Health</button>
+        <button onClick={() => handleCategoryChange('science')} className="mr-2 p-4 shadow-black shadow-2xl  bg-rose-800 text-white rounded-full">Science</button>
+        <button onClick={() => handleCategoryChange('sports')} className="mr-2 p-4 shadow-black shadow-2xl  bg-rose-800 text-white rounded-full">Sports</button>
+        <button onClick={() => handleCategoryChange('entertainment')} className="p-4 shadow-black shadow-2xl  bg-rose-800 text-white rounded-full">Entertainment</button>
+      </div>
+
+      <ul className=' bg-[#7A2048] p-6 text-white rounded-2xl shadow-2xl border shadow-black'>
+        {articles.map((article, index) => (
+          <li key={index} className="mb-4">
+            <img className=' mb-2 h-20 rounded-3xl' src={article.urlToImage} alt="" />
+            <h2 className="text-xl font-semibold">{article.title}</h2>
+            <p className='py-2'>{article.description}</p>
+            <a href={article.url} target="_blank" rel="noopener noreferrer" className=" bg-red-800 text-white p-2 rounded-lg ">
+              Read more
+            </a>
+            <hr className='mt-4 h-0.5 bg-black' />
+          </li>
+          
+        ))}
+      </ul>
+      <center>
+      <div className=" my-10">
+        <button
+          onClick={() => handlePageChange(page - 1)}
+          disabled={page === 1}
+          className="mr-2 p-2 bg-blue-900 text-white rounded disabled:bg-gray-800"
+        >
+          Previous  
+        </button>
+        <span><b>Page  {page}</b></span>
+        <button
+          onClick={() => handlePageChange(page + 1)}
+          disabled={articles.length < pageSize}
+          className="ml-2 p-2 bg-red-900 text-white rounded disabled:bg-gray-300"
+        >
+          Next
+        </button>
+      </div>
+      </center>
+    </div>
+    
+  );
+};
+
+export default News;
---
+++ src/navbar.jsx
@@ -0,0 +1,25 @@
+export default function Navbar(){
+    return(
+        <section>
+        <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <button href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <img src="https://persistventure.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ff78663f8-78b0-4675-9f1f-807e68f53ff1%2Fd21112dc-736f-4db3-959a-6304909957dc%2FLogo_green_png.png?table=block&id=b12625b9-cb25-4feb-96fb-fe5162121c4d&spaceId=f78663f8-78b0-4675-9f1f-807e68f53ff1&width=250&userId=&cache=v2" class="h-16" alt="NeighborGood" />
+      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">NeighborGood</span>
+  </button  >
+  <div className="hidden md:flex gap-20">
+  <a className="text-white" href="https://github.com/metaBlocks2003"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg></a>
+
+<a className="text-white" href="https://www.linkedin.com/in/aditya-sengupta-5308a52a5/"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg></a>
+  </div>
+
+        </div>
+  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+    <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+    </ul>
+  </div>
+</nav>
+
+        </section>
+    )
+}
